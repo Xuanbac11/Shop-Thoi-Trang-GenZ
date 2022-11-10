a4cefb5e7f85d9040a3310ca89a3d02fe86a92cd
Huy Mr <huy339093@gmail.com>
2022-11-10 23:32:36 +0700

    test mp4

--- TestCode/app/src/main/java/com/example/testcode/MainActivity.java
+++ TestCode/app/src/main/java/com/example/testcode/MainActivity.java
@@ -6,16 +6,21 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+  WebView oggy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = findViewById(R.id.tv);
-        tv.setText("Siu nhân gao");
-        tv.setTextColor(Color.RED);
+        oggy=new WebView(this);
+        oggy.getSettings().setJavaScriptEnabled(true);
+        String link="<iframe width=\"380\" height=\"320\" src=\"https://www.youtube.com/embed/xno-D8RCq1w\" title=\"Tik Tok\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+        oggy.loadData(link,"text/html", null);
+        setContentView(oggy);
+
     }
 }
